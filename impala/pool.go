c31e348f34d413a49426d09a82cbe5639aaf49ad
Fedomn <shadowforfiend@gmail.com>
2018-03-26 22:54:20 +0800

    add conn pool

---
+++ impala/pool.go
@@ -0,0 +1,57 @@
+package impala
+
+import (
+	"fmt"
+	"github.com/jolestar/go-commons-pool"
+)
+
+type PoolConfig struct {
+	Host                          string
+	Port                          int
+	MaxTotal                      int
+	MaxIdle                       int
+	MinIdle                       int
+	MaxWaitMillis                 int64
+	MinEvictableIdleTimeMillis    int64
+	TimeBetweenEvictionRunsMillis int64
+}
+
+type Pool struct {
+	cfg PoolConfig
+}
+
+func (p *Pool) MakeObject() (*pool.PooledObject, error) {
+	conn, err := NewConn(p.cfg.Host, p.cfg.Port)
+	return pool.NewPooledObject(conn), err
+}
+
+func (p *Pool) DestroyObject(obj *pool.PooledObject) error {
+	conn, ok := obj.Object.(*Conn)
+	if !ok {
+		return fmt.Errorf("assert conn error, ojb: %+v", obj.Object)
+	}
+	return conn.Close()
+}
+
+func (p *Pool) ValidateObject(object *pool.PooledObject) bool {
+	return true
+}
+
+func (p *Pool) ActivateObject(object *pool.PooledObject) error {
+	return nil
+}
+
+func (p *Pool) PassivateObject(object *pool.PooledObject) error {
+	return nil
+}
+
+func InitPool(cfg PoolConfig) *pool.ObjectPool {
+	p := pool.NewObjectPoolWithDefaultConfig(&Pool{cfg})
+	p.Config.MaxTotal = cfg.MaxTotal
+	p.Config.MaxIdle = cfg.MaxIdle
+	p.Config.MaxWaitMillis = cfg.MaxWaitMillis
+	p.Config.MinEvictableIdleTimeMillis = cfg.MinEvictableIdleTimeMillis
+	p.Config.TimeBetweenEvictionRunsMillis = cfg.TimeBetweenEvictionRunsMillis
+	p.StartEvictor()
+	return p
+}
---
+++ impala/pool_test.go
@@ -0,0 +1,61 @@
+package impala_test
+
+import (
+	. "fedomn/common/impala"
+	"fmt"
+	"github.com/jolestar/go-commons-pool"
+	"testing"
+	"time"
+)
+
+var p *pool.ObjectPool
+
+func TestMain(m *testing.M) {
+	cfg := PoolConfig{
+		Host:                          "120.27.220.47",
+		Port:                          21000,
+		MaxTotal:                      2,
+		MaxIdle:                       1,
+		MinIdle:                       0,
+		MaxWaitMillis:                 1000 * 2,
+		MinEvictableIdleTimeMillis:    1000 * 3,
+		TimeBetweenEvictionRunsMillis: 1000,
+	}
+	p = InitPool(cfg)
+	m.Run()
+}
+
+func TestEvictIdle(t *testing.T) {
+	fmt.Printf("Init Pool Conn Active: %d, Idel: %d\n", p.GetNumActive(), p.GetNumIdle())
+	conn1, _ := p.BorrowObject()
+	p.BorrowObject()
+	fmt.Printf("Get conn1, conn2. Pool Conn Active: %d, Idel: %d\n", p.GetNumActive(), p.GetNumIdle())
+
+	p.ReturnObject(conn1)
+	fmt.Printf("Return conn1. Pool Conn Active: %d, Idel: %d\n", p.GetNumActive(), p.GetNumIdle())
+
+	fmt.Println("time sleep 5s")
+	time.Sleep(time.Second * 5)
+
+	fmt.Printf("Conn Active: %d, Idel: %d\n", p.GetNumActive(), p.GetNumIdle())
+}
+
+func TestMaxWaitMillis(t *testing.T) {
+	fmt.Printf("Init Pool Conn Active: %d, Idel: %d\n", p.GetNumActive(), p.GetNumIdle())
+	p.BorrowObject()
+	p.BorrowObject()
+	conn, err := p.BorrowObject()
+	fmt.Println(conn, err)
+}
+
+func TestMaxIdle(t *testing.T) {
+	fmt.Printf("Init Pool Conn Active: %d, Idel: %d\n", p.GetNumActive(), p.GetNumIdle())
+	conn1, _ := p.BorrowObject()
+	conn2, _ := p.BorrowObject()
+	p.ReturnObject(conn1)
+	p.ReturnObject(conn2)
+
+	fmt.Printf("conn1: %+v\n", conn1)
+	fmt.Printf("conn2: %+v\n", conn2)
+	fmt.Printf("Conn Active: %d, Idel: %d\n", p.GetNumActive(), p.GetNumIdle())
+}
